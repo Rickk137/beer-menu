aa7f1ea6806edb2ab031ee762f224dfceac9e0c7
peymannaderi3 <peymannaderi3@yahoo.com>
2020-11-05 15:11:58 +0330

    Loading added.

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import Beer from "../Beer";
+import Loading from "../Loading";
 
 // styles
 import classes from "./index.module.scss";
@@ -12,9 +13,16 @@ import { getBeers } from "../../services/apiService";
 
 const Items = () => {
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
   const getItems = async () => {
-    const { data } = await getBeers();
-    setList(data);
+    setLoading(true);
+    try {
+      const { data } = await getBeers();
+      setList(data);
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -28,6 +36,11 @@ const Items = () => {
       justifyContent="space-around"
       p={1}
     >
+      {loading && (
+        <div className={classes.loading}>
+          <Loading />
+        </div>
+      )}
       {list.map((item) => (
         <Beer item={item} key={item.id} />
       ))}
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,17 @@
+const Loading = () => {
+  return (
+    <div className="lds-grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
